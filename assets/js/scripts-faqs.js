a3590f5aa029963eb5316f81d24a3f74a4af74b7
Eduarda Lanzarini <eduarda06032007@gmail.com>
2024-09-13 14:01:31 -0300

    Update scripts-faqs.js

--- assets/js/scripts-faqs.js
+++ assets/js/scripts-faqs.js
@@ -1,1 +1,13 @@
-//console.log("FAQS - Olá, Mundo!");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const faqs = document.querySelectorAll('.faq');
+
+    faqs.forEach((faq) => {
+        const question = faq.querySelector('.faq-question');
+        const answer = faq.querySelector('.faq-answer');
+
+        question.addEventListener('click', (event) => {
+            const isAnswerVisible = getComputedStyle(answer).display !== 'none';
+            answer.style.display = isAnswerVisible ? 'none' : 'block';
+        });
+    });
+});
